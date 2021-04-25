68af98977f1b4b7f03d4ae28cf8d62d353fb46a8
gusbdev <gusbdev@gmail.com>
2021-04-25 18:19:22 -0300

    Alteração de teste

--- numero_negativo.py
+++ numero_negativo.py
@@ -2,7 +2,7 @@ numero = float(input('Informe um numero: '))
 
 if numero < 0:
     
-    print(f'O numero {numero} é negativo')
+    print(f'O número {numero} é negativo')
 
 else:
     print(f'O nuemro {numero} é positivo')
